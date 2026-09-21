75481c1ac50fc843446a15802003d1edea4fbfc0
= <=>
2026-09-21 20:18:56 +0700

    membuat dan menampilkan halaman user

--- user.php
+++ user.php
@@ -18,7 +18,7 @@ $id_tamu = $huruf . sprintf("%03s", $urutan);
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">Buku Tamu</h1>
+    <h1 class="h3 mb-4 text-gray-800">Data User</h1>
 
     <?php
     // jika ada tombol simpan
@@ -46,7 +46,7 @@ $id_tamu = $huruf . sprintf("%03s", $urutan);
                 <span class="icon text-white-50">
                     <i class="fas fa-plus"></i>
                 </span>
-                <span class="text">Data Tamu</span>
+                <span class="text">Data User</span>
             </button>
         </div>
         <div class="card-body">
@@ -56,46 +56,26 @@ $id_tamu = $huruf . sprintf("%03s", $urutan);
                         <tr>
                             <th>No</th>
                             <th>Tanggal</th>
-                            <th>Nama Tamu</th>
-                            <th>Alamat</th>
-                            <th>No. Telp/HP</th>
-                            <th>Bertemu Dengan</th>
-                            <th>Kepentingan</th>
+                            <th>User Role</th>
                             <th>Aksi</th>
                         </tr>
                     </thead>
-                    <tfoot>
-                        <tr>
-                            <th>No</th>
-                            <th>Tanggal</th>
-                            <th>Nama Tamu</th>
-                            <th>Alamat</th>
-                            <th>No. Telp/HP</th>
-                            <th>Bertemu Dengan</th>
-                            <th>Kepentingan</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </tfoot>
                     <tbody>
                         <?php
                         // penomoran auto-increment
                         $no = 1;
-                        // query untuk memanggil semua data dari table buku-tamu
-                        $bukutamu = query("SELECT * FROM bukutamu");
-                        foreach ($bukutamu as $tamu) :
+                        // query untuk memanggil semua data dari table users
+                        $users = query("SELECT * FROM users");
+                        foreach ($users as $user) :
                         ?>
                             <tr>
                                 <td><?= $no++; ?></td>
-                                <td><?= $tamu['tanggal'] ?></td>
-                                <td><?= $tamu['nama_tamu'] ?></td>
-                                <td><?= $tamu['alamat'] ?></td>
-                                <td><?= $tamu['no_hp'] ?></td>
-                                <td><?= $tamu['bertemu'] ?></td>
-                                <td><?= $tamu['kepentingan'] ?></td>
+                                <td><?= $user['username'] ?></td>
+                                <td><?= $user['user_role'] ?></td>
                                 <td>
-                                    <a class="btn btn-success" href="edit-tamu.php?id=<?= $tamu['id_tamu'] ?>">Ubah</a>
+                                    <a class="btn btn-success" href="edit-tamu.php?id=<?= $user['id_user'] ?>">Ubah</a>
                                     <a onclick="confirm('Apakah anda yakin ingin menghapus data ini')" class="btn btn-danger"
-                                        href="hapus-tamu.php?id=<?= $tamu['id_tamu'] ?>">Hapus</a>
+                                        href="hapus-tamu.php?id=<?= $user['id_user'] ?>">Hapus</a>
                                 </td>
                             </tr>
                         <?php endforeach; ?>
