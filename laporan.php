ace2105a3650f9d89b2f5539e459c1997fd0fdba
= <=>
2026-09-23 09:27:19 +0700

    menambahkan fitur export laporan format excel

--- laporan.php
+++ laporan.php
@@ -1,6 +1,19 @@
 <?php
 include_once('templates/header.php');
 include('function.php');
+
+
+if (isset($_POST['tampilkan'])) {
+    $p_awal = $_POST['p_awal'];
+    $p_akhir = $_POST['p_akhir'];
+
+    $link = "export-laporan.php?cari=true&p_awal=$p_awal&p_akhir=$p_akhir";
+    //query sesuai dengan keywoard
+    $bukutamu = query("SELECT * FROM bukutamu WHERE tanggal BETWEEN 'p_awal' AND 'p_akhir'");
+} else {
+    //query ambil semua data buku tamu
+    $bukutamu = query("SELECT * FROM bukutamu ORDER BY tanggal DESC");
+}
 ?>
 
 <!-- Begin Page Content -->
@@ -54,6 +67,14 @@ include('function.php');
 
     <div class="card shadow mb-4">
         <div class="card-header py-3">
+            <a href="<?= isset($_POST['tampilkan']) ? $link : 'export-laporan.php'; ?>" target="_blank" class="btn btn-success btn-icon-split">
+                <span class="icon text-white-50">
+                    <i class="fas fa-file-excel"></i>
+                </span>
+                <span class="text">Export Laporan</span>
+            </a>
+        </div>
+        <div class="card-header py-3">
             <span class="text">Tabel Histori Tamu</span>
         </div>
         <div class="card-body">
@@ -73,29 +94,27 @@ include('function.php');
                     </thead>
                     <tbody>
                         <?php
-                        if (isset($_POST['tampilkan'])) {
-                            $p_awal = $_POST['p_awal'];
-                            $p_akhir = $_POST['p_akhir'];
-                            $no = 1;
-                            $bukutamu = query("SELECT * FROM bukutamu WHERE tanggal BETWEEN '$p_awal' AND '$p_akhir'");
-                            foreach ($bukutamu as $tamu) : ?>
-                                <tr>
-                                    <td><?= $no++; ?></td>
-                                    <td><?= $tamu['tanggal'] ?></td>
-                                    <td><?= $tamu['nama_tamu'] ?></td>
-                                    <td><?= $tamu['alamat'] ?></td>
-                                    <td><?= $tamu['no_hp'] ?></td>
-                                    <td><?= $tamu['bertemu'] ?></td>
-                                    <td><?= $tamu['kepentingan'] ?></td>
-                                    <td>
-                                        <a class="btn btn-success" href="edit-tamu.php?id=<?= $tamu['id_tamu'] ?>">Ubah</a>
-                                        <a onclick="return confirm('Apakah anda yakin ingin menghapus data ini?')"
-                                            class="btn btn-danger"
-                                            href="hapus-tamu.php?id=<?= $tamu['id_tamu'] ?>">Hapus</a>
-                                    </td>
-                                </tr>
+                        // penomoran auto-increment
+                        $no = 1;
+
+                        foreach ($bukutamu as $tamu) :
+                        ?>
+                            <tr>
+                                <td><?= $no++; ?></td>
+                                <td><?= $tamu['tanggal'] ?></td>
+                                <td><?= $tamu['nama_tamu'] ?></td>
+                                <td><?= $tamu['alamat'] ?></td>
+                                <td><?= $tamu['no_hp'] ?></td>
+                                <td><?= $tamu['bertemu'] ?></td>
+                                <td><?= $tamu['kepentingan'] ?></td>
+                                <td>
+                                    <a class="btn btn-success" href="edit-tamu.php?id=<?= $tamu['id_tamu'] ?>">Ubah</a>
+                                    <a onclick="return confirm('Apakah anda yakin ingin menghapus data ini?')"
+                                        class="btn btn-danger"
+                                        href="hapus-tamu.php?id=<?= $tamu['id_tamu'] ?>">Hapus</a>
+                                </td>
+                            </tr>
                         <?php endforeach;
-                        }
                         ?>
                     </tbody>
                 </table>
